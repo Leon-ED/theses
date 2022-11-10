3f1502fcad89a7d84c54f45f4e1d88ee0ca8404f
Leon-ED <edmeeleon@outlook.fr>
2022-11-10 16:33:56 +0100

    changement formulation dashboard

--- include/dashboard.inc.php
+++ include/dashboard.inc.php
@@ -1,25 +1,25 @@
 <section class="dashboard">
     <div class="card widget-flat text-center" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Nombre de thèses</h5>
+            <h5 class="card-title">Thèses(s)</h5>
             <h1 class="card-subtitle mt-3 mb-3 text-muted"><?= $nombre_theses ?></h1>
         </div><?= $nombre_  ?>
     </div>
     <div class="card widget-flat text-center" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Nombre d'auteurs</h5>
+            <h5 class="card-title">Auteur(s)</h5>
             <h1 class="card-subtitle mt-3 mb-3 text-muted"><?= $nombre_auteurs ?></h1>
         </div>
     </div>
     <div class="card widget-flat text-center" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Nombre de directeurs</h5>
+            <h5 class="card-title">Directeur(s)</h5>
             <h1 class="card-subtitle mt-3 mb-3 text-muted"><?= $nombre_directeurs  ?></h1>
         </div>
     </div>
     <div class="card widget-flat text-center" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Etablissements</h5>
+            <h5 class="card-title">Etablissement(s)</h5>
             <h1 class="card-subtitle mt-3 mb-3 text-muted"><?= $nombre_etablissements  ?></h1>
         </div>
     </div>
