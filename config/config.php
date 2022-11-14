35ad97cf171e2af7a3f2e09fc861c9b7bc2a9c12
Leon-ED <edmeeleon@outlook.fr>
2022-11-14 22:08:55 +0100

    del sudo

--- config/config.php
+++ config/config.php
@@ -8,13 +8,7 @@ define("DEBUG", false);
 if (PHP_SESSION_NONE === session_status()) {
     session_start();
 }
-if (isset($_GET["sudo"])) {
-    $_SESSION["user"] = "ee";
-}
 
-if (!isset($_SESSION['user']) && $file != "login.php" && $file != "register.php") {
-    header('Location: ../view/login.php');
-}
 if (DEBUG === true) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
