50509e87a1091dec210ffb9866c7bde190e4bb57
= <=>
2023-04-07 23:43:30 +0200

    affichage mot de passe ou identifiant incorrect sur le login et compte deja pris sur le regisdter

--- view/login.php
+++ view/login.php
@@ -1,11 +1,16 @@
 <?php
 require_once("./config/config.php");
 require_once("./include/html.header.inc.php");
+$erreur = "";
+if(isset($_GET['error'])){
+    $erreur = "Les identifiants sont incorrects ou le compte n'existe pas";
+}
 ?>
 
 <body>
     <?php require_once("./include/nav.inc.php") ?>
     <h1 class="text-center" style="margin-top:1%;">Se connecter</h1>
+    <p class="text-center" style="margin-top:1%;"><?php echo $erreur?></p>
     <form action="controller/authController.php" method="POST" class="authForm">
         <div class="form-container">
             <label for="login">Nom d'utilisateur</label>
--- view/register.php
+++ view/register.php
@@ -1,10 +1,16 @@
 <?php
 require_once("./config/config.php");
 require_once("./include/html.header.inc.php");
+$erreur = "";
+if(isset($_GET['error'])){
+    $erreur = "Une erreur est survenue lors de l'inscription, un compte avec ce nom d'utilisateur ou cette adresse courriel existe peut-être  déjà";
+}
 ?>
 <body>
 <?php require_once("./include/nav.inc.php") ?>
 <h1 class="text-center" style="margin-top:1%;">S'inscrire</h1>
+<p class="text-center" style="margin-top:1%;"><?php echo $erreur?></p>
+
 <form action="controller/authController.php" method="POST" class="authForm">
     <div class="form-container">
         <label for="username">Nom d'utilisateur</label>
